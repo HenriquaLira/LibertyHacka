2019bcf4f6ab9fbb7980d69c988878965c84d781
Henrique Lira <HenriquA_GTI@prestadorliberty.com.br>
2019-12-01 05:24:22 -0300

    Novas funções

--- PainelAnalyse/Controllers/HomeController.cs
+++ PainelAnalyse/Controllers/HomeController.cs
@@ -1,17 +1,19 @@
 ﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+using System;
 using System.IO;
 using System.Net;
 using System.Web.Mvc;
+using JsonRes = PainelAnalyse.Models;
 
 namespace Painel_de_Verificação.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
 
-            HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format("https://maps.googleapis.com/maps/api/geocode/json?address=Rua%20Doutor%20Gentil%20Leite%20Martins%20152&key=AIzaSyAlZA6KjskEkGM7m7z0SsfKAE6kGLucTWA"));
+            HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format("https://maps.googleapis.com/maps/api/geocode/json?address=Rua%20Francisco%20Rebelo%20321&key=AIzaSyAlZA6KjskEkGM7m7z0SsfKAE6kGLucTWA"));
             WebReq.Method = "GET";
             HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
 
@@ -22,7 +24,36 @@ namespace Painel_de_Verificação.Controllers
                 jsonString = reader.ReadToEnd();
             }
 
-            //Results results = JsonConvert.DeserializeObject<Results>(jsonString);
+            JsonRes.JsonResult jsonResults = JsonConvert.DeserializeObject<JsonRes.JsonResult>(jsonString);
+            Tuple<double, double> latLong = _returnLongLat(jsonResults);
+
+            ViewData["Message"] = String.Format("https://maps.googleapis.com/maps/api/streetview?size=600x300&location={0},{1}&heading=151.78&pitch=-0.76&key=AIzaSyAlZA6KjskEkGM7m7z0SsfKAE6kGLucTWA", latLong.Item1, latLong.Item2);
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string tipo, string endereco, string numero, string CEP)
+        {
+
+            string enderecoFormatado = _formatarEndereco(tipo, endereco, numero, CEP);
+
+            HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format("https://maps.googleapis.com/maps/api/geocode/json?address={0}&key=AIzaSyAlZA6KjskEkGM7m7z0SsfKAE6kGLucTWA", enderecoFormatado));
+            WebReq.Method = "GET";
+            HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
+
+            string jsonString;
+            using (Stream stream = WebResp.GetResponseStream())   //modified from your code since the using statement disposes the stream automatically when done
+            {
+                StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+                jsonString = reader.ReadToEnd();
+            }
+
+            JsonRes.JsonResult jsonResults = JsonConvert.DeserializeObject<JsonRes.JsonResult>(jsonString);
+            Tuple<double, double> latLong = _returnLongLat(jsonResults);
+
+            ViewData["Message"] = String.Format("https://maps.googleapis.com/maps/api/streetview?size=600x300&location={0},{1}&heading=151.78&pitch=-0.76&key=AIzaSyAlZA6KjskEkGM7m7z0SsfKAE6kGLucTWA", latLong.Item1, latLong.Item2);
+            ViewData["linkMaps"] = String.Format("https://www.google.com.br/maps/place/{0}", enderecoFormatado);
 
             return View();
         }
@@ -40,12 +71,20 @@ namespace Painel_de_Verificação.Controllers
 
             return View();
         }
-    }
 
-    //[J("status")] (Description = "results")]
-    //public class Results
-    //{
-    //    [JsonProperty(PropertyName = "formatted_address")]
-    //    public string formatted_address { get; set; }
-    //}
+        private Tuple<double, double> _returnLongLat(JsonRes.JsonResult jsonResults)
+        {
+            Tuple<double, double> tuple = 
+                new Tuple<double, double>(jsonResults.Results[0].Geometry.Location.Lat, 
+                jsonResults.Results[0].Geometry.Location.Lng);
+
+            return tuple;
+        }
+        private string _formatarEndereco(string tipo, string endereco, string numero, string CEP)
+        {
+            string enderecoFormatado = String.Format("{0}%20{1}%20{2}%20{3}", tipo, endereco, numero, CEP);
+
+            return enderecoFormatado;
+        }
+    }
 }
---
+++ PainelAnalyse/Models/JsonResult.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+
+namespace PainelAnalyse.Models
+{
+    public class JsonResult
+    {
+        [JsonProperty("results")]
+        public Result[] Results { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+
+    public class Result
+    {
+        [JsonProperty("address_components")]
+        public AddressComponent[] AddressComponents { get; set; }
+
+        [JsonProperty("formatted_address")]
+        public string FormattedAddress { get; set; }
+
+        [JsonProperty("geometry")]
+        public Geometry Geometry { get; set; }
+
+        [JsonProperty("place_id")]
+        public string PlaceId { get; set; }
+
+        [JsonProperty("plus_code")]
+        public PlusCode PlusCode { get; set; }
+
+        [JsonProperty("types")]
+        public string[] Types { get; set; }
+    }
+
+    public class AddressComponent
+    {
+        [JsonProperty("long_name")]
+        public string LongName { get; set; }
+
+        [JsonProperty("short_name")]
+        public string ShortName { get; set; }
+
+        [JsonProperty("types")]
+        public string[] Types { get; set; }
+    }
+
+    public class Geometry
+    {
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+
+        [JsonProperty("location_type")]
+        public string LocationType { get; set; }
+
+        [JsonProperty("viewport")]
+        public Viewport Viewport { get; set; }
+    }
+
+    public class Location
+    {
+        [JsonProperty("lat")]
+        public double Lat { get; set; }
+
+        [JsonProperty("lng")]
+        public double Lng { get; set; }
+    }
+
+    public class Viewport
+    {
+        [JsonProperty("northeast")]
+        public Location Northeast { get; set; }
+
+        [JsonProperty("southwest")]
+        public Location Southwest { get; set; }
+    }
+
+    public class PlusCode
+    {
+        [JsonProperty("compound_code")]
+        public string CompoundCode { get; set; }
+
+        [JsonProperty("global_code")]
+        public string GlobalCode { get; set; }
+    }
+}
